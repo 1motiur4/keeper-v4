350f9f99560c4520e1a873cd0d5a9baf202e4474
Motiur Muhith <1motiur4@gmail.com>
2023-09-11 19:00:22 -0400

    beep

--- client/src/assets/wrappers/Landing.js
+++ client/src/assets/wrappers/Landing.js
@@ -2,6 +2,16 @@ import styled from "styled-components";
 
 const Wrapper = styled.section`
   text-align: center;
+
+  button {
+    background-color: lightblue;
+    font-size: 3em;
+  }
+
+  .login-btn {
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 10px;
+  }
 `;
 
 export default Wrapper;
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,8 +1,42 @@
 import Wrapper from "../assets/wrappers/Landing";
 
 const Landing = () => {
-  return <Wrapper>
-    test
-  </Wrapper>
+  return (
+    <Wrapper>
+      <div className="box">
+        <div className="buttons-container">
+          <button
+            type="button"
+            name="login-btn"
+            className="login-btn"
+          >
+            Login
+          </button>
+          <button
+            type="button"
+            name="register-btn"
+            className="register-btn"
+          >
+            Register
+          </button>
+        </div>
+        <form className="form-div">
+          <label htmlFor="login">Login</label>
+          <input
+            type="text"
+            name="login"
+          />
+          <br></br>
+          <label htmlFor="register">Register</label>
+          <input
+            type="text"
+            name="register"
+          />
+          <br></br>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </Wrapper>
+  );
 };
 export default Landing;
