1db6a74d05a727cfa0c2dc5ceea29a409b3b817f
Motiur Muhith <1motiur4@gmail.com>
2023-09-20 17:01:40 -0400

    began auth stuff

---
+++ models/User.js
@@ -0,0 +1,38 @@
+import mongoose, { mongo } from "mongoose";
+import validator from "validator";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongooose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please provide first name"],
+    minlength: 1,
+    maxlength: 20,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please provide last name"],
+    minlength: 1,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide a valid email",
+    },
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+    select: false,
+  },
+});
+
+export default mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 
+//routers
+import authRouter from "./routers/authRouter.js";
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send({ msg: "Welcome!" });
 });
@@ -10,8 +15,8 @@ app.get("/api/v1", (req, res) => {
   res.json({ msg: "API" });
 });
 
+app.use("/api/v1/auth", authRouter);
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
-
-
