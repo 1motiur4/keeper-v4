cd9b352bd8fb64d51884f50e76c3e971323426d3
Motiur Muhith <1motiur4@gmail.com>
2023-09-26 16:59:00 -0400

    duplicate error

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,21 @@
 import User from "../models/User.js";
 
 export const register = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+
+  if (!firstName || !lastName || !email || !password) {
+    res.status(400).json({ msg: "Please provide all values!" });
+  }
+
+  const userAlreadyExists = await User.findOne({ email });
+  if (userAlreadyExists) {
+    res.status(400).json({ msg: "Email already in use!" });
+  }
+
   const user = await User.create(req.body);
   res.status(200).json({ user });
 };
+
 export const login = async (req, res) => {
   res.send("login");
 };
