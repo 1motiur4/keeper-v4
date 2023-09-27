9f1d570b6ec10abdf8755159114fb7eae46d9017
Motiur Muhith <1motiur4@gmail.com>
2023-09-27 18:24:52 -0400

    hmm

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,11 @@ import User from "../models/User.js";
 
 export const register = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
+  console.log(req.body);
 
   if (!firstName || !lastName || !email || !password) {
-    return res.status(400).json({ msg: "Please provide all values!" });
+    // return res.status(400).json({ msg: "Please provide all values!" });
+    throw new Error("Please provide all values");
   }
 
   const userAlreadyExists = await User.findOne({ email });
